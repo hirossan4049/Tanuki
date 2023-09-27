3540f19939c091589798fb3a265d279149b5a93c
hirossan4049 <haruto405329@gmail.com>
2023-09-28 01:37:15 +0900

    Initial Commit

---
+++ tanuki/tanukiApp.swift
@@ -0,0 +1,17 @@
+//
+//  tanukiApp.swift
+//  tanuki
+//
+//  Created by a on 9/28/23.
+//
+
+import SwiftUI
+
+@main
+struct tanukiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
